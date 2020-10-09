ee462ae2e47f443de1a103b89c8bbbdc95f2daa5
Renato <renato-afonso@usp.br>
2020-10-09 19:47:20 -0300

    adicionado manipulacao de atributos no model

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,8 +4,56 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Blog extends Model
 {
     use HasFactory;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::retrieved(function($model){
+        });
+        
+        self::saving(function($model){
+
+        });
+
+        self::saved(function($model){
+            // ... code here
+        });
+        self::creating(function($model){
+
+        });
+        self::created(function($model){
+            // ... code here
+        });
+
+        self::updating(function($model){
+            // ... code here
+        });
+
+        self::updated(function($model){
+            // ... code here
+        });
+        self::deleting(function($model){
+            // ... code here
+        });
+
+        self::deleted(function($model){
+            // ... code here
+        });
+    }
+
+    public function getCreatedAtAttribute($value) {
+        return \Carbon\Carbon::parse($value)->format('d/m/Y - H:i');
+    }
+
+    public function getTituloAttribute($value) { 
+        return Str::ucfirst($value);
+    }
+
+    
 }
